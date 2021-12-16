56d967e784327bca14ab5ef41799c1fb35ca0273
Robert Young <robertyoung10101@gmail.com>
2021-12-16 21:53:28 +1300

    Remove cmd files from gitignore

---
+++ cmd/api-service/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"os"
+	"strings"
+
+	passport "github.com/leeprovoost/go-rest-api-template/internal/passport"
+	vparse "github.com/leeprovoost/go-rest-api-template/pkg/version"
+	log "github.com/sirupsen/logrus"
+	"github.com/unrolled/render"
+)
+
+func init() {
+	if "LOCAL" == strings.ToUpper(os.Getenv("ENV")) {
+		log.SetFormatter(&log.TextFormatter{})
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetFormatter(&log.JSONFormatter{})
+		log.SetLevel(log.InfoLevel)
+	}
+}
+
+func main() {
+	// ===========================================================================
+	// Load environment variables
+	// ===========================================================================
+	var (
+		env     = strings.ToUpper(os.Getenv("ENV")) // LOCAL, DEV, STG, PRD
+		port    = os.Getenv("PORT")                 // server traffic on this port
+		version = os.Getenv("VERSION")              // path to VERSION file
+	)
+	// ===========================================================================
+	// Read version information
+	// ===========================================================================
+	version, err := vparse.ParseVersionFile(version)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"env":  env,
+			"err":  err,
+			"path": os.Getenv("VERSION"),
+		}).Fatal("Can't find a VERSION file")
+		return
+	}
+	log.WithFields(log.Fields{
+		"env":     env,
+		"path":    os.Getenv("VERSION"),
+		"version": version,
+	}).Info("Loaded VERSION file")
+	// ===========================================================================
+	// Initialise data storage
+	// ===========================================================================
+	userStore := passport.NewUserService(passport.CreateMockDataSet())
+	betStore := passport.NewBetService(passport.CreateMockBets())
+	// ===========================================================================
+	// Initialise application context
+	// ===========================================================================
+	appEnv := passport.AppEnv{
+		Render:    render.New(),
+		Version:   version,
+		Env:       env,
+		Port:      port,
+		UserStore: userStore,
+		BetStore:  betStore,
+	}
+	// ===========================================================================
+	// Start application
+	// ===========================================================================
+	passport.StartServer(appEnv)
+}
